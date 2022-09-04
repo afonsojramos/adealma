20944928536e094d5cf422d234f06242b2bc4a25
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 23:07:27 +0100

    feat(Logo): component styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,27 +1,12 @@
 import { useTranslation } from 'next-i18next';
-import Image from 'next/image';
 
-type ILogoProps = {
-  xl?: boolean;
-};
-
-const Logo = (props: ILogoProps) => {
-  const size = props.xl ? '44' : '32';
-  const fontStyle = props.xl
-    ? 'font-semibold text-3xl'
-    : 'font-semibold text-xl';
+const Logo = () => {
   const { t } = useTranslation('common');
-
   return (
-    <span className={`text-gray-900 inline-flex items-center ${fontStyle}`}>
-      <Image
-        src="/assets/icons/black.png"
-        alt="logo"
-        width={size}
-        height={size}
-      ></Image>
-
-      <span className="pt-1">{t('site_name')}</span>
+    <span
+      className={`text-gray-900 inline-flex items-center tracking-[.15em] font-bold`}
+    >
+      {t('site_name')}
     </span>
   );
 };
