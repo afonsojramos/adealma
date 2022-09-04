a2fa56fd27967b25dec0ae915119c672a1dc4cf9
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 18:18:04 +0100

    feat(Hero): component styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,13 @@
-import Image from 'next/image';
+type IHeroProps = {
+  description: string;
+};
 
-import matilde from '../../public/assets/icons/android-chrome-512x512.png';
-import { Section } from './elements/Section';
-
-const Hero = () => (
-  <Section yPadding="bg-base-20">
-    <div className="hero-content flex-col lg:flex-row lg:mt-20">
-      <Image
-        alt=""
-        src={matilde}
-        width={450}
-        height={450}
-        className="rounded-full"
-      />
-      <div className="flex-1 flex flex-col justify-center lg:ml-10">
-        <h1 className="text-7xl font-baseline font-['Mogan'] leading-hero mb-10">
-          Hello
-        </h1>
-        <div className="text-base mt-4 lg:mb-13">
-          <div className="text-2xl font-black mb-2">
-            This will be a short description title
-          </div>
-          <span className="leading-6">Lorem Ipsum and whatnot</span>
-        </div>
-      </div>
+const Hero = ({ description }: IHeroProps) => {
+  return (
+    <div className="justify-center leading-[44px] lg:text-4xl md:text-3xl text-2xl lg:px-10">
+      {description}
     </div>
-  </Section>
-);
+  );
+};
 
 export { Hero };
