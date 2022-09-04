cb1bfea323365e7dac2c2237e5c306b4eced91ee
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 18:17:12 +0100

    feat: create `Banner` component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,22 @@
-import { Section } from './elements';
+import A from '../../public/assets/a.svg';
+import Alma from '../../public/assets/alma.svg';
+import De from '../../public/assets/de.svg';
 
-const Banner = () => (
-  <Section yPadding="py-1">
-    <div className="text-center p-4">
-      <p>Banner Thing</p>
-      <label className="btn btn-primary m-10">click me!</label>
+const Banner = () => {
+  const common = 'fixed -z-10 h-20 xl:h-48 lg:h-40 md:h-32 sm:h-24';
+  return (
+    <div className="font-bold text-8xl sm:text-9xl md:text-11xl lg:text-12xl xl:text-13xl tracking-[0.2em]">
+      <A
+        className={`left-6 lg:left-8 xl:left-12 2xl:left-16 top-6 ${common}`}
+      />
+      <De
+        className={`right-6 lg:right-8 xl:right-12 2xl:right-16 inset-y-1/2 -mt-12 ${common}`}
+      />
+      <Alma
+        className={`bottom-6 lg:bottom-8 xl:bottom-12 2xl:bottom-16 left-6 lg:inset-x-1/2 lg:-ml-80 ${common}`}
+      />
     </div>
-  </Section>
-);
+  );
+};
 
 export { Banner };
