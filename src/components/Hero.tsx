3cc8b85cc4657f1ff6c84730aaecc513926482ee
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 23:07:00 +0100

    feat(Hero): component styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,8 @@ type IHeroProps = {
 
 const Hero = ({ description }: IHeroProps) => {
   return (
-    <div className="justify-center leading-[44px] lg:text-4xl md:text-3xl text-2xl lg:px-10">
-      {description}
+    <div className="justify-center leading-[28px] sm:leading-[34px] md:leading-[44px] xl:leading-[54px] w-fit text-2xl sm:text-3xl md:text-4xl xl:text-5xl px-8 sm:pr-48 md:pr-64 lg:pr-[24rem] xl:pr-[36rem] 2xl:pr-[48rem] lg:pl-12 pb-48">
+      <p>{description}</p>
     </div>
   );
 };
