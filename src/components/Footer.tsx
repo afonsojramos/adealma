df4ef06d3e76701478309eb0fce165a59f1b0626
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 23:06:31 +0100

    feat(Footer): component styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,39 +1,16 @@
-import { Section } from './elements';
-import { FooterCopyright } from './elements/FooterCopyright';
-import { FooterIconList } from './elements/FooterIconList';
-
-const Footer = () => (
-  <Section>
-    <div className="relative flex items-center pb-16">
-      <div className="flex-grow border-t border-gray-400"></div>
+const Footer = () => {
+  return (
+    <div className="p-10 text-sm md:text-lg">
+      <p className="sm:pl-32">
+        <span>@a_dealma</span>
+        <span className="pl-16 sm:pl-32 lg:pl-64">info@adealma.com</span>
+      </p>
+      <p className="text-right sm:text-left sm:pl-64 lg:pl-96">
+        <span>adealma.com</span>
+        <span className="pl-16 sm:pl-32 lg:pl-64">+351 935 658 89</span>
+      </p>
     </div>
-    <footer className="footer footer-center p-10 text-base-content rounded">
-      {/*  <Logo /> */}
-
-      {/* <div className="grid grid-flow-col gap-4">
-        <Link href="/">
-          <a className="link link-hover">Home</a>
-        </Link>
-        <Link href="/">
-          <a className="link link-hover">Resume</a>
-        </Link>
-        <Link href="/">
-          <a className="link link-hover">Projects</a>
-        </Link>
-        <Link href="/">
-          <a className="link link-hover">Contacts</a>
-        </Link>
-      </div> */}
-      <div>
-        <div className="grid grid-flow-col gap-4">
-          <FooterIconList />
-        </div>
-      </div>
-      <div>
-        <FooterCopyright />
-      </div>
-    </footer>
-  </Section>
-);
+  );
+};
 
 export { Footer };
