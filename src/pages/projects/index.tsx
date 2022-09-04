045e7c689a6e70fb6109012f5a5d8f99929793ee
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 23:13:03 +0100

    chore(projects): initial page styling

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,8 +3,7 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
 
-import { Navbar, Banner, Footer } from '../../components';
-import { Meta, Background } from '../../components/elements';
+import { Navbar, Meta } from 'Components';
 
 const Projects = (props: {
   projects: {
@@ -16,65 +15,71 @@ const Projects = (props: {
   }[];
 }) => {
   const { t } = useTranslation('common');
+  const projectsIntro = t<string, string[]>('projects', {
+    returnObjects: true,
+  });
   return (
-    <main className="antialiased text-gray-600">
-      <Meta title={t('title_about')} description={t('description')} />
-      <Background color="bg-gray-100" classnames="h-screen">
-        <Navbar />
-        <div className="inline-block w-full rounded-lg relative overflow-x-auto px-10">
-          <table className="table-fixed w-full">
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
-                ></th>
-                <th
-                  scope="col"
-                  className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
-                >
-                  Location
-                </th>
-                <th
-                  scope="col"
-                  className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
-                >
-                  Type
-                </th>
-                <th
-                  scope="col"
-                  className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
-                >
-                  Date
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {props.projects.map((project) => (
-                <tr key={project.slug}>
-                  <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    <Link href={`/projects/${project.slug}`}>
-                      {project.frontmatter.title}
-                    </Link>
-                  </td>
-                  <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    {project.frontmatter.location}
-                  </td>
-                  <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    {project.frontmatter.status}
-                  </td>
-                  <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    {project.frontmatter.year}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <>
+      <Meta title={t('title_projects')} description={t('description')} />
+      <Navbar title={t('title_projects')} />
+      <main className="inline-block w-full mt-32 lg:mt-48 rounded-lg relative overflow-x-auto">
+        <div className="pl-8 md:pl-16 lg:pl-32 pr-32 md:pr-32 lg:pr-64 xl:pr-96 2xl:pr-[40rem] leading-[28px] md:leading-[30px] xl:leading-[40px] text-[22px] md:text-2xl xl:text-3xl line-clamp-5 hover:line-clamp-none mb-12">
+          {projectsIntro.map((paragraph, pNum) => (
+            <p key={pNum} className="mb-2">
+              {paragraph}
+            </p>
+          ))}
         </div>
-      </Background>
-      <Banner />
-      <Footer />
-    </main>
+        <table className="table-fixed w-screen">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
+              ></th>
+              <th
+                scope="col"
+                className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
+              >
+                Location
+              </th>
+              <th
+                scope="col"
+                className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
+              >
+                Type
+              </th>
+              <th
+                scope="col"
+                className="bg-gray-100 text-left text-xs font-bold text-gray-600 uppercase tracking-wider"
+              >
+                Date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.projects.map((project) => (
+              <tr key={project.slug}>
+                <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  <Link href={`/projects/${project.slug}`}>
+                    {project.frontmatter.title}
+                  </Link>
+                </td>
+                <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  {project.frontmatter.location}
+                </td>
+                <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  {project.frontmatter.status}
+                </td>
+                <td className="px-10 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                  {project.frontmatter.year}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </>
   );
 };
 
