a2348ec52cbeaceb349ddfbaa041b390d088be6a
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 18:17:26 +0100

    chore: remove `Section` component

--- src/components/elements/index.tsx
+++ src/components/elements/index.tsx
@@ -1,7 +1,5 @@
-export * from './Background';
 export * from './FooterCopyright';
 export * from './FooterIconList';
 export * from './HeroOneButton';
 export * from './Meta';
-export * from './Section';
 export * from './VerticalFeatureRow';
