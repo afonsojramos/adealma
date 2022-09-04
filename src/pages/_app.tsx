3452e1d9518d1c490d5e9fbcff4c04191f4317f0
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 23:10:48 +0100

    fix: change importing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 
-import '../styles/global.css';
+import 'styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
