a56cce74afeca11d3ecbd56e218acb8555cd3c48
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-04 15:08:55 +0100

    chore: removed `fr` locale & set basic copy

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'pt', 'fr'],
+    locales: ['en', 'pt'],
   },
   ...(typeof window === 'undefined'
     ? { localePath: path.resolve('./public/locales') }
