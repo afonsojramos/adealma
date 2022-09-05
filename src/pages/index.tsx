82551b1e14d552c037df2d003bc871469046374b
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-05 09:54:46 +0100

    feat(landing): add swipeable navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import config from 'next/config';
 import Link from 'next/link';
+import Router from 'next/router';
+import { useSwipeable } from 'react-swipeable';
 
 import { Navbar, Hero, Meta, Banner, Footer } from 'Components';
 
@@ -9,20 +12,25 @@ import Arrow from '../../public/assets/arrow.svg';
 const Index = () => {
   const { t } = useTranslation('common');
   const description = t('description');
+  const handlers = useSwipeable({
+    onSwipedUp: (eventData) =>
+      eventData.deltaY < -200 && Router.replace('/projects'),
+    ...config,
+  });
   return (
     <>
       <Meta title={t('title')} description={description} />
       <Navbar />
-      <main className="flex flex-col">
+      <main className="flex flex-col" {...handlers}>
         <div className="grow h-screen">
           <Banner />
         </div>
-        <div className="flex flex-col bg-gray-100 w-full pb-32">
-          <div className="relative">
+        <div className="flex flex-col bg-gray-100 w-full">
+          <div id="scrollable" className="relative">
             <Hero description={description} />
             <Link href="/projects">
               <a>
-                <Arrow className="absolute h-8 rotate-180 lg:right-14 lg:inset-y-1/4" />
+                <Arrow className="absolute h-6 md:h-8 rotate-180 right-6 lg:right-8 xl:right-12 2xl:right-14 bottom-2 md:inset-y-1/4" />
               </a>
             </Link>
           </div>
