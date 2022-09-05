127cc8a53f436e4422e7b3db7f2909c910e14cbb
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-05 10:01:14 +0100

    tweak(Hero): mobile bottom padding

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -4,7 +4,7 @@ type IHeroProps = {
 
 const Hero = ({ description }: IHeroProps) => {
   return (
-    <div className="justify-center leading-[28px] sm:leading-[34px] md:leading-[44px] xl:leading-[54px] w-fit text-2xl sm:text-3xl md:text-4xl xl:text-5xl px-8 sm:pr-48 md:pr-64 lg:pr-[24rem] xl:pr-[36rem] 2xl:pr-[48rem] lg:pl-12 pb-48">
+    <div className="justify-center leading-[28px] sm:leading-[34px] md:leading-[44px] xl:leading-[54px] w-fit text-2xl sm:text-3xl md:text-4xl xl:text-5xl px-8 pr-12 sm:pr-48 md:pr-64 lg:pr-[24rem] xl:pr-[36rem] 2xl:pr-[48rem] lg:pl-12 pb-36 md:pb-48">
       <p>{description}</p>
     </div>
   );
