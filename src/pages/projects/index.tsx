6d24e993d8e9b82e5a44ba4f409402fc9c8f473c
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-11 23:48:59 +0100

    tweak(Projects): update paddings and mobile table

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -53,25 +53,27 @@ const Projects = ({ projects }: { projects: IProject[] }) => {
   return (
     <>
       <Meta title={t('projects_title')} description={t('description')} />
-      <Navbar title={t('projects_title')} />
-      <main className="inline-block w-full mt-32 lg:mt-48 rounded-lg overflow-x-auto">
+      <div className="child:bg-primary-100">
+        <Navbar title={t('projects_title')} />
+      </div>
+      <main className="inline-block w-full mt-32 lg:mt-32 rounded-lg overflow-x-auto">
         <Link href={`/`}>
           <a>
             <Arrow className="absolute left-8 h-8 hidden lg:block" />
           </a>
         </Link>
-        <div className="pl-8 md:pl-16 lg:pl-32 pr-32 md:pr-32 lg:pr-64 xl:pr-96 2xl:pr-[40rem] leading-[28px] md:leading-[30px] xl:leading-[40px] text-[22px] md:text-2xl xl:text-3xl line-clamp-5 hover:line-clamp-none mb-12">
+        <div className="pl-8 lg:pl-24 xl:pl-48 pr-32 md:pr-32 lg:pr-64 xl:pr-96 2xl:pr-[35rem] leading-[28px] md:leading-[30px] xl:leading-[40px] text-[22px] md:text-2xl xl:text-3xl mb-12 break-normal">
           {projectsDescription.map((paragraph, pNum) => (
             <p key={pNum} className="mb-2">
               {paragraph}
             </p>
           ))}
         </div>
-        <table className="table-fixed w-screen text-left">
+        <table className="table-fixed w-screen text-left border-spacing-2">
           <thead>
             <tr className="border-b-[1px] border-primary-900 tracking-widest text-lg child:font-light">
-              <th className="hidden md:table-cell md:w-80 lg:w-[29%]" />
-              <th className="hidden md:table-cell md:w-40 lg:w-[31%] lg:pl-20">
+              <th className="hidden md:table-cell md:w-[36%]" />
+              <th className="hidden md:table-cell md:w-[27%]">
                 <button
                   className="group inline-flex items-center leading-10"
                   onClick={() =>
@@ -90,7 +92,7 @@ const Projects = ({ projects }: { projects: IProject[] }) => {
                   {t('location')}
                 </button>
               </th>
-              <th className="hidden md:table-cell md:w-32 lg:w-[20%] px-5">
+              <th className="hidden md:table-cell md:w-[20%] px-5">
                 <button
                   className="group inline-flex items-center leading-10"
                   onClick={() =>
@@ -109,7 +111,7 @@ const Projects = ({ projects }: { projects: IProject[] }) => {
                   {t('status')}
                 </button>
               </th>
-              <th className="flex items-center md:w-40 lg:w-3/12 pl-8 md:px-5">
+              <th className="flex items-center md:w-3/12 pl-8 md:px-5">
                 <button
                   className="group inline-flex items-center leading-10"
                   onClick={() =>
@@ -131,16 +133,14 @@ const Projects = ({ projects }: { projects: IProject[] }) => {
           <tbody>
             {projects.map((project) => (
               <Link key={project.slug} href={`/projects/${project.slug}`}>
-                <tr className="border-b-[1px] border-primary-900 tracking-widest text-2xl child:py-2 group child-hover:cursor-image hover:bg-primary-300 hover:text-primary-100">
-                  <td className="hidden md:table-cell pl-16 lg:pl-32 pr-16 lg:pr-64">
+                <tr className="border-b-[1px] border-primary-900 tracking-widest text-2xl child:py-2 group hover:bg-primary-300 hover:text-primary-100 hidden md:table-row">
+                  <td className="hidden md:table-cell pl-8 lg:pl-24 xl:pl-48 pr-16 lg:pr-64">
                     <div className="flex flex-row items-center w-max">
                       <span>{project.title}</span>
-                      <LinkChain className="px-2 w-max opacity-0 group-hover:opacity-100 transform transition duration-500" />
+                      <LinkChain className="px-2 w-max opacity-0 group-hover:opacity-100 transform transition duration-500 text-primary-100" />
                     </div>
                   </td>
-                  <td className="hidden md:table-cell lg:pl-20">
-                    {project.location}
-                  </td>
+                  <td className="hidden md:table-cell">{project.location}</td>
                   <td className="hidden md:table-cell px-5">
                     {t(project.status)}
                   </td>
@@ -148,22 +148,33 @@ const Projects = ({ projects }: { projects: IProject[] }) => {
                     <span className="hidden md:table-cell">
                       {getProjectYear(project.date)}
                     </span>
-                    <button
-                      className="md:hidden"
-                      onClick={() => expandYear(project.date)}
-                    >
-                      {getProjectYear(project.date)}
-                    </button>
                   </td>
                 </tr>
               </Link>
+            ))}
+            {projects.map((project) => (
+              <tr
+                key={project.slug}
+                className="border-b-[1px] border-primary-900 tracking-widest text-2xl child:py-2 hover:bg-primary-300 hover:text-primary-100 md:hidden"
+              >
+                <td className="text-center md:text-left px-5">
+                  <button className="" onClick={() => expandYear(project.date)}>
+                    {getProjectYear(project.date)}
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
         <div className="flex flex-row-reverse w-screen justify-between">
           <Link href={`/projects/${projects[0]?.slug}`}>
             <a>
-              <Arrow className="h-6 md:h-8 m-8 rotate-180" />
+              <Arrow className="h-6 m-8 rotate-180" />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <Arrow className="h-6 m-8 flex lg:hidden" />
             </a>
           </Link>
         </div>
