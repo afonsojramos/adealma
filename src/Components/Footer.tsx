23297c0ba8fcf09500bbe9ee860ec621a8b1324f
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-11 23:43:39 +0100

    tweaks(Footer): update text size & padding

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <div className="p-10 md:pt-20 md:text-lg">
+    <div className="p-10 md:pt-20 text-sm md:text-lg">
       <p className="sm:pl-32">
         <span>@a_dealma</span>
         <span className="pl-16 sm:pl-32 lg:pl-64">info@adealma.com</span>
       </p>
-      <p className="text-right sm:text-left sm:pl-64 lg:pl-96">
+      <p className="text-right sm:text-left sm:pl-64 lg:pl-96 pt-2">
         <span>adealma.com</span>
         <span className="pl-16 sm:pl-32 lg:pl-64">+351 935 658 89</span>
       </p>
