6b376e3c4fefba3b5588f4dc8580046250523eb3
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-09-12 04:38:37 +0100

    fix(Icons): import paths

--- src/Components/Icons.tsx
+++ src/Components/Icons.tsx
@@ -1,7 +1,7 @@
-import Arrow from '../../../public/assets/arrow.svg';
-import LinkChain from '../../../public/assets/link-chain.svg';
 import A from '../../public/assets/a.svg';
 import Alma from '../../public/assets/alma.svg';
+import Arrow from '../../public/assets/arrow.svg';
 import De from '../../public/assets/de.svg';
+import LinkChain from '../../public/assets/link-chain.svg';
 
 export { A, Alma, Arrow, De, LinkChain };
