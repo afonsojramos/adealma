589fca237ab8289b3f9ac4f8c2719785f999da16
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-09-27 12:59:30 +0100

    fix(Footer): add links and change to inline-flex

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,14 +1,22 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <footer className="p-10 md:pt-20 text-sm md:text-lg bg-primary-100">
-      <p className="sm:pl-32">
-        <span>@a_dealma</span>
-        <span className="pl-16 sm:pl-32 lg:pl-64">info@adealma.com</span>
-      </p>
-      <p className="text-right sm:text-left sm:pl-64 lg:pl-96 pt-2">
-        <span>adealma.com</span>
-        <span className="pl-16 sm:pl-32 lg:pl-64">+351 935 658 89</span>
-      </p>
+      <div className="inline-flex sm:pl-32">
+        <div>@a_dealma</div>
+        <div className="inline-flex pl-16 sm:pl-32 lg:pl-64">
+          <Link href="mailto:formulaobliqua@gmail.com">info@adealma.com</Link>
+        </div>
+      </div>
+      <div className="text-right sm:text-left sm:pl-64 lg:pl-96 pt-2">
+        <div className="inline-flex">
+          <Link href="/">adealma.com</Link>
+        </div>
+        <div className="inline-flex pl-16 sm:pl-32 lg:pl-64">
+          <Link href="tel:+35193565889">+351 935 658 89</Link>
+        </div>
+      </div>
     </footer>
   );
 };
