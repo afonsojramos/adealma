09de6d1ead13c58daafb248cc37c5c3686f1cd35
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-09-29 12:55:03 +0100

    fix(Footer): add spacing between elements to improve SEO readability

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <footer className="p-10 md:pt-20 text-sm md:text-lg bg-primary-100">
       <div className="inline-flex sm:pl-32">
-        <div>@a_dealma</div>
+        <div>@a_dealma</div>{' '}
         <div className="inline-flex pl-16 sm:pl-32 lg:pl-64">
           <Link href="mailto:formulaobliqua@gmail.com">info@adealma.com</Link>
         </div>
-      </div>
+      </div>{' '}
       <div className="text-right sm:text-left sm:pl-64 lg:pl-96 pt-2">
         <div className="inline-flex">
           <Link href="/">adealma.com</Link>
-        </div>
+        </div>{' '}
         <div className="inline-flex pl-16 sm:pl-32 lg:pl-64">
           <Link href="tel:+35193565889">+351 935 658 89</Link>
         </div>
