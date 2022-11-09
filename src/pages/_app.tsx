7561dbe1857122922f6bca8e957506e44aa99e1f
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-11-09 04:24:23 +0000

    chore: setup analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 
 import 'styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default appWithTranslation(MyApp);
