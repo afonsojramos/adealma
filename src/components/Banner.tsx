53c725af9799c9f8eb992e6f6eb547848c14c783
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-11-16 20:35:06 +0000

    chore: remove unused font

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import { A, De, Alma } from './Icons';
 const Banner = () => {
   const common = 'fixed -z-10 h-20 xl:h-48 lg:h-40 md:h-32 sm:h-24';
   return (
-    <div className="font-bold text-8xl sm:text-9xl md:text-11xl lg:text-12xl xl:text-13xl tracking-[0.2em]">
+    <>
       <A
         className={`left-6 lg:left-8 xl:left-12 2xl:left-16 top-6 ${common}`}
       />
@@ -13,7 +13,7 @@ const Banner = () => {
       <Alma
         className={`bottom-6 lg:bottom-8 xl:bottom-12 2xl:bottom-16 left-6 lg:inset-x-1/2 lg:-ml-80 ${common}`}
       />
-    </div>
+    </>
   );
 };
 
