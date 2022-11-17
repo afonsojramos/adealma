eb2e6135d9681cf623b1ebc17149b990f90ceb8b
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-11-17 13:47:00 +0000

    chore: add top padding to prevent CLS

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ type IHeroProps = {
 };
 
 const Hero = ({ description }: IHeroProps) => (
-  <div className='w-fit justify-center px-8 pr-12 pb-36 pt-2 text-2xl leading-[28px] sm:pr-48 sm:pt-0 sm:text-3xl sm:leading-[34px] md:pr-64 md:pb-48 md:text-4xl md:leading-[44px] lg:pr-[24rem] lg:pl-12 xl:pr-[36rem] xl:text-5xl xl:leading-[54px] 2xl:pr-[48rem]'>
+  <div className='w-fit justify-center px-8 pr-12 pb-36 pt-4 text-2xl leading-[28px] sm:pr-48 sm:text-3xl sm:leading-[34px] md:pr-64 md:pb-48 md:text-4xl md:leading-[44px] lg:pr-[24rem] lg:pl-12 xl:pr-[36rem] xl:text-5xl xl:leading-[54px] 2xl:pr-[48rem]'>
     {description}
   </div>
 );
