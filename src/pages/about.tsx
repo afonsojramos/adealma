f9b86ae1dfc2545ad41820de9b7ebd1a54e52fbf
Afonso Ramos <afonsojorgeramos@gmail.com>
2023-02-16 04:48:00 +0000

    feat: add carousel to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
+import Carousel from 'components/Carousel';
 import Footer from 'components/Footer';
 import Meta from 'components/Meta';
 import Navbar from 'components/Navbar';
@@ -9,6 +9,9 @@ import Navbar from 'components/Navbar';
 const About = () => {
   const { t } = useTranslation('common');
   const about = t('about_description', {
+    returnObjects: true,
+  });
+  const images = t('about_images', {
     returnObjects: true,
   });
   return (
@@ -20,32 +23,7 @@ const About = () => {
         <div className='mb-12 break-normal px-8 text-xl leading-[28px] md:pr-32 md:text-2xl md:leading-[30px] lg:pl-24 lg:pr-64 xl:pl-48 xl:pr-96 xl:text-3xl xl:leading-[40px] 2xl:pr-[35rem]'>
           {about[0]}
         </div>
-        <div className='flex w-screen flex-row items-center px-5'>
-          <Image
-            src='/assets/casa-do-cristelo.png'
-            alt='Casa do Cristelo'
-            width={820}
-            height={1260}
-            className='w-1/3 px-2'
-            priority
-          />
-          <Image
-            src='/assets/casa-do-pescador.png'
-            alt='Casa do Pescador'
-            width={820}
-            height={1260}
-            className='w-1/3 px-2'
-            priority
-          />
-          <Image
-            src='/assets/casas-da-matriz.png'
-            alt='Casas da Matriz'
-            width={820}
-            height={1260}
-            className='w-1/3 px-2'
-            priority
-          />
-        </div>
+        <Carousel images={images} />
         <div className='mb-12 break-normal px-8 pt-16 text-xl leading-[28px] md:pr-32 md:text-2xl md:leading-[30px] lg:pl-24 lg:pr-64 xl:pl-48 xl:pr-96 xl:text-3xl xl:leading-[40px] 2xl:pr-[35rem]'>
           {about[1]}
         </div>
