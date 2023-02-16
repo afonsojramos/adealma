0f2dc15f1fff48df120347cbdc7ab4be34d3503a
Afonso Ramos <afonsojorgeramos@gmail.com>
2023-02-16 14:16:23 +0000

    fix: footer for narrow screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,18 @@ import Link from 'next/link';
 
 const Footer = ({ about = 'about' }) => (
   <footer className='bg-primary-100 p-10 text-sm md:pt-20 md:text-lg'>
-    <div className='inline-flex sm:pl-16 md:pl-24 lg:pl-32'>
-      <div>
+    <div className='sm:pl-16 md:pl-24 lg:pl-32'>
+      <div className='inline-flex pb-2'>
         <Link href='https://www.instagram.com/adealma.studio'>
           @adealma.studio
         </Link>
       </div>
-      <div className='inline-flex pl-16 sm:pl-32 lg:pl-64'>
+      <div className='inline-flex pl-4 sm:pl-24 lg:pl-64'>
         <Link href='mailto:formulaobliqua@gmail.com'>
           formulaobliqua@gmail.com
         </Link>
       </div>
-      <div className='inline-flex pl-8 font-bold sm:pl-28 lg:pl-64 xl:pl-96'>
+      <div className='inline-flex pl-8 font-bold sm:pl-24 lg:pl-64 xl:pl-96'>
         <Link href='/about'>{about}</Link>
       </div>
     </div>{' '}
@@ -21,7 +21,7 @@ const Footer = ({ about = 'about' }) => (
       <div className='inline-flex'>
         <Link href='/'>adealma.com</Link>
       </div>{' '}
-      <div className='inline-flex pl-16 pr-8 sm:pr-0 sm:pl-32 lg:pl-64'>
+      <div className='inline-flex pl-8 pr-0 sm:pl-32 lg:pl-64'>
         <Link href='tel:+351936780872'>+351 936 780 872</Link>
       </div>
     </div>
